a57f1f73e8a0ab684ecc85206408581f9850fbcf
Basant joshi <bsntballabhjoshi@gmail.com>
2023-09-02 20:24:47 +0530

    jwt added

--- LoginForm/Routes/users.js
+++ LoginForm/Routes/users.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const Router = express.Router();
 const CreateUser = require('../Schema/CreateUser');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
 
 //api link  /api/createuser.bod
 Router.post('/createuser',async(req,res)=>{
-    const { name , password , address , city } = req.body
+    const { name ,password, address , city } = req.body;
+
+    const securepass = await bcrypt.hash(password, saltRounds);;
+
     const user = new CreateUser({
         name: name,
-        password: password,
+        password: securepass,
         address : address,
         city: city
     });
+
+    const token = await user.generatetoken();
+    
+
     const result = await user.save();
     console.log(result)
     res.send(result)
--- LoginForm/Schema/CreateUser.js
+++ LoginForm/Schema/CreateUser.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+var jwt = require('jsonwebtoken');
+const SECURE_KEY = process.env.SECURE_KEY;
 
 const regester = mongoose.Schema({
     name:{
@@ -12,7 +14,23 @@ const regester = mongoose.Schema({
     },
     city:{
         type: String
-    }
+    },
+    tokens:[
+      {  token:{
+            type: String,
+            required: true
+        }}
+    ]
+    
 })
+//middleware
+regester.methods.generatetoken = async function(){
+    console.log(this._id);
+const tokengen = await jwt.sign({_id : this._id.toString()},"jklwdqwldnqwkdnqwldnk");
+this.tokens = this.tokens.concat({token:tokengen})
+console.log(tokengen);
+return tokengen;
+}
+
 const createuser = mongoose.model("Createuser", regester);
 module.exports = createuser;
